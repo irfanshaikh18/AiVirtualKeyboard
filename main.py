218c1b911c508d1d92d76c5294b7553ffafcb3f6
Irfan Shaikh <irfansk.dev@gmail.com>
2021-10-03 20:01:03 +0530

    ai virtual keyboard project completes

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import cv2
 from cvzone.HandTrackingModule import HandDetector
+from time import sleep
+import cvzone
 
 cap = cv2.VideoCapture(0)
 cap.set(3, 1280)
@@ -18,6 +20,8 @@ def drawALL(img, buttonList):
     for button in buttonList:
         x, y = button.pos
         w, h = button.size
+        cvzone.cornerRect(img, (button.pos[0], button.pos[1], button.size[0], button.size[1]),
+                          20, rt=0)
         cv2.rectangle(img, button.pos, (x + w, y + h), (255, 0, 255), cv2.FILLED)
         cv2.putText(img, button.text, (x + 20, y + 65), cv2.FONT_HERSHEY_PLAIN, 4, (255, 255, 255), 4)
 
@@ -54,11 +58,14 @@ while True:
                 l, _, _ = detector.findDistance(8, 12, img)
                 print(l)
 
+                # when clicked
                 if l < 30:
                     cv2.rectangle(img, button.pos, (x + w, y + h), (0, 255, 0), cv2.FILLED)
                     cv2.putText(img, button.text, (x + 20, y + 65), cv2.FONT_HERSHEY_PLAIN, 4, (255, 255, 255), 4)
+                    finalText += button.text
+                    sleep(0.4)
 
-    cv2.rectangle(img, (50, 350), (700, 450), (175, 0, 175), cv2.FILLED)
+    cv2.rectangle(img, (50, 350), (900, 450), (175, 0, 175), cv2.FILLED)
     cv2.putText(img, finalText, (60, 425), cv2.FONT_HERSHEY_PLAIN, 5, (255, 255, 255), 5)
 
     cv2.imshow("Image", img)
